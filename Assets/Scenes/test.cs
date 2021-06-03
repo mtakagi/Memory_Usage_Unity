4a06f6152633749aa288a3b151ef08f8f544d4fa
mtakagi <mtakagi127@live.jp>
2021-06-04 03:49:42 +0900

    Add macOS Plugin

---
+++ Assets/Scenes/test.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var usage = MemoryUsage.PhysicalUsage();
+        Debug.Log($"{usage / (1024 * 1024)}MB");
+    }
+}
---
+++ Assets/com.0xabadbabe.memory_usage/Runtime/Scripts/MemoryUsage.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+public static class MemoryUsage
+{
+    [DllImport("memory_usage")]
+    private static extern long memory_usage();
+
+    public static long PhysicalUsage()
+    {
+        return memory_usage();
+    }
+}
