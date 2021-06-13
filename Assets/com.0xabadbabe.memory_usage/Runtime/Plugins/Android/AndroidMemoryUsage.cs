7c7617bde802e6daadd588c3e742f5c8aea3d75c
mtakagi <mtakagi127@live.jp>
2021-06-13 20:51:02 +0900

    add android plugin

---
+++ Assets/com.0xabadbabe.memory_usage/Runtime/Plugins/Android/AndroidMemoryUsage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class AndroidMemoryUsage
+{
+
+    private static string activityService;
+
+    static AndroidMemoryUsage()
+    {
+        using (var activityClass = new AndroidJavaClass("android.app.Activity"))
+        {
+            activityService = activityClass.GetStatic<string>("ACTIVITY_SERVICE");
+        }
+    }
+    public static int MemoryUsage()
+    {
+        using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+        using (var manager = activity.Call<AndroidJavaObject>("getSystemService", activityService))
+        using (var process = new AndroidJavaClass("android.os.Process"))
+        {
+            var pid = new int[1] { process.CallStatic<int>("myPid") };
+            var memoryInfo = manager.Call<AndroidJavaObject[]>("getProcessMemoryInfo", (object)pid);
+            var getTotalPss = memoryInfo[0].Call<int>("getTotalPss");
+
+            memoryInfo[0].Dispose();
+
+            return getTotalPss;
+        }
+    }
+}
