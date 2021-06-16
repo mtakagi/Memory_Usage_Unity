dc313adb7fa606c955aff1efdf7f927b109d7f08
mtakagi <mtakagi127@live.jp>
2021-06-17 02:42:02 +0900

    add android plugin (#1)

--- Assets/Scenes/test.cs
+++ Assets/Scenes/test.cs
@@ -4,15 +4,15 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
- public class test : MonoBehaviour
+public class test : MonoBehaviour
 {
-    #if UNITY_IOS && !UNITY_EDITOR
+#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
     [DllImport("__Internal")]
     private static extern void allocation();
 
     [DllImport("__Internal")]
     private static extern void deallocation();
-    #endif
+#endif
 
     [SerializeField]
     private Text memoryUsage;
@@ -20,21 +20,23 @@ using System.Runtime.InteropServices;
     // Update is called once per frame
     void Update()
     {
+#if UNITY_IOS || UNITY_ANDROID || UNITY_EDITOR_OSX
         var usage = MemoryUsage.PhysicalUsage();
         memoryUsage.text = $"{usage / (1024 * 1024)}MB";
+#endif
     }
 
     public void Malloc()
     {
-        #if UNITY_IOS && !UNITY_EDITOR
+#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
         allocation();
-        #endif
+#endif
     }
 
-    public void Free() 
+    public void Free()
     {
-        #if UNITY_IOS && !UNITY_EDITOR
+#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
         deallocation();
-        #endif
+#endif
     }
 }
--- Assets/com.0xabadbabe.memory_usage/Runtime/Scripts/MemoryUsage.cs
+++ Assets/com.0xabadbabe.memory_usage/Runtime/Scripts/MemoryUsage.cs
@@ -2,15 +2,22 @@
 
 public static class MemoryUsage
 {
-    #if UNITY_IOS && !UNITY_EDITOR
+#if UNITY_IOS && !UNITY_EDITOR
     [DllImport("__Internal")]
-    #else
-    [DllImport("memory_usage")]
-    #endif
     private static extern long memory_usage();
+#elif UNITY_EDITOR_OSX
+    [DllImport("memory_usage")]
+    private static extern long memory_usage();
+#endif
 
     public static long PhysicalUsage()
     {
+#if UNITY_IOS || UNITY_EDITOR_OSX
         return memory_usage();
+#elif UNITY_ANDROID && !UNITY_EDITOR
+        return AndroidMemoryUsage.MemoryUsage() * 1024;
+#else
+        throw new System.Exception();
+#endif
     }
 }
