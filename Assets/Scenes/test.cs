91da0cfc6abd58ab250c5bc5e03e507cc75e86eb
mtakagi <mtakagi127@live.jp>
2022-05-22 19:40:22 +0900

    add macOS support (#4)

--- Assets/Scenes/test.cs
+++ Assets/Scenes/test.cs
@@ -8,11 +8,17 @@ public class test : MonoBehaviour
 {
 #if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
     [DllImport("__Internal")]
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+    [DllImport("allocation")]
+#endif
     private static extern void allocation();
 
+#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
     [DllImport("__Internal")]
-    private static extern void deallocation();
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+    [DllImport("allocation")]
 #endif
+    private static extern void deallocation();
 
     [SerializeField]
     private Text memoryUsage;
@@ -20,22 +26,29 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-#if UNITY_IOS || UNITY_ANDROID || UNITY_EDITOR_OSX
-        var usage = MemoryUsage.PhysicalUsage();
-        memoryUsage.text = $"{usage / (1024 * 1024)}MB";
+#if UNITY_IOS || UNITY_ANDROID || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        try
+        {
+            var usage = MemoryUsage.PhysicalUsage();
+            memoryUsage.text = $"{usage / (1024 * 1024)}MB";
+        }
+        catch (Exception e)
+        {
+            memoryUsage.text = e.Message;
+        }
 #endif
     }
 
     public void Malloc()
     {
-#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
+#if UNITY_IOS || UNITY_ANDROID || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
         allocation();
 #endif
     }
 
     public void Free()
     {
-#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
+#if UNITY_IOS || UNITY_ANDROID || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX 
         deallocation();
 #endif
     }
--- Assets/com.0xabadbabe.memory_usage/Runtime/Scripts/MemoryUsage.cs
+++ Assets/com.0xabadbabe.memory_usage/Runtime/Scripts/MemoryUsage.cs
@@ -5,14 +5,14 @@ public static class MemoryUsage
 #if UNITY_IOS && !UNITY_EDITOR
     [DllImport("__Internal")]
     private static extern long memory_usage();
-#elif UNITY_EDITOR_OSX
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
     [DllImport("memory_usage")]
     private static extern long memory_usage();
 #endif
 
     public static long PhysicalUsage()
     {
-#if UNITY_IOS || UNITY_EDITOR_OSX
+#if UNITY_IOS || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
         return memory_usage();
 #elif UNITY_ANDROID && !UNITY_EDITOR
         return AndroidMemoryUsage.MemoryUsage() * 1024;
